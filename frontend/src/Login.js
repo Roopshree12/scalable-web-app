ee7108a3333e19909f8580af0e2b7bd1d6e68d7d
Roopshree Nagar <nagarroopshree@gmail.com>
2026-01-02 15:44:55 +0530

    Login.js URL

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -12,7 +12,7 @@ function Login() {
     console.log("LOGIN CLICKED");
 
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://scalable-web-app-auth-dashboard.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
